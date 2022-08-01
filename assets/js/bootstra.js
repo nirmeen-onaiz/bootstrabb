c051af4928837b8bcea38d09107f56ab6c720290
nirmeen onez <nirmeenonaiz@gmail.com>
2022-08-01 16:18:51 -0700

    add five events

---
+++ assets/js/bootstra.js
@@ -0,0 +1,27 @@
+function order(){
+
+document.write("you need a burger ...");
+
+}
+function orderPepsi()
+{
+    document.write("you need a pepsi ...");
+}
+function searchOf()
+{
+    document.write("Searching.....");
+
+}
+function selectt()
+{
+   document.write("You selected some text") ;
+}
+
+function change()
+{
+    document.write("you change number of people");
+}
+
+function myFunction(x) {
+    x.style.background = "green";
+  }
